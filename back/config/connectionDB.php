f19d8d55928e721a2f837b141ecadfe212698d0c
sofiagapr <80787807+sofiagapr@users.noreply.github.com>
2023-05-03 20:54:52 -0500

    Update

---
+++ back/config/connectionDB.php
@@ -0,0 +1,18 @@
+<?php
+//Developer: Mario Zambrano 
+$host ="127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "pets";
+$port = "3307";
+$conn = new mysqli($host,$username,$password,$dbname,$port);
+
+if($conn->connect_error){
+ echo "conexion fallida ".$conn->connect_error;
+
+}else{
+    echo"conexcion satisfactoria";
+}
+
+
+?>
---
+++ back/src/login.php
@@ -0,0 +1,16 @@
+<?php
+    include("../config/connectionDB.php");  
+    $email = $_POST['email'];
+    $passwd = md5($_POST['password']);
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$passwd'"; 
+    $result = $conn->query($sql);
+    if($result->num_rows > 0){
+        //echo"el usuario existe ";
+        header("location:http://127.0.0.1/pets_m/front/src/home.html");
+    }else{
+        //echo"el usuario no existe o datos incorrectos";
+        //echo $conn->error;
+        echo"<script>alert('Usuario no existe o datos son incorrectos'  )</script>";
+        header("refresh:0; url=http://127.0.0.1/pets_m/front/src/login.html");
+    }
+?>
